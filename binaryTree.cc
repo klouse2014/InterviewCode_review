cfd525a31d5ba94ac2b08bde1b2e031569b723b9
klouse <klouse@126.com>
2014-03-25 08:29:55 +0800

    排序＋二叉树遍历

--- binaryTree.cc
+++ binaryTree.cc
@@ -65,6 +65,59 @@ void inOrder(binaryTreeNode *root)
   cout<<endl;
 }
 
+void postOrder(binaryTreeNode *root)
+{
+  stack<binaryTreeNode *> stk;
+  stack<int> stk_symbol;
+  while(root != NULL || !stk.empty())
+  {
+    while(root != NULL)
+    {
+      stk.push(root);
+      stk_symbol.push(0);
+      root = root->lchild;
+    }
+    if(!stk.empty())
+    {
+      int temp_symbol = stk_symbol.top();
+      binaryTreeNode *temp = stk.top();
+      if(temp_symbol == 0)
+      {
+        stk_symbol.pop();
+        stk_symbol.push(1);
+        root = temp->rchild;
+      }
+      else
+      {
+        cout<<temp->m_value<<" ";
+        stk.pop();
+        stk_symbol.pop();
+        root = NULL;
+      }
+    }
+  }
+  cout<<endl;
+}
+
+void levelOrder(binaryTreeNode *root)
+{
+  if(root == NULL)
+    return;
+  queue<binaryTreeNode *> que;
+  que.push(root);
+  while(!que.empty())
+  {
+    binaryTreeNode *temp = que.front();
+    que.pop();
+    cout<<temp->m_value<<" ";
+    if(temp->lchild != NULL)
+      que.push(temp->lchild);
+    if(temp->rchild != NULL)
+      que.push(temp->rchild);
+  }
+  cout<<endl;
+}
+
 int main()
 {
   binaryTreeNode *root = NULL;
@@ -72,6 +125,8 @@ int main()
 
   preOrder(root);
   inOrder(root);
+  postOrder(root);
+  levelOrder(root);
   return 0;
 }
 
